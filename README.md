8817f3bb803d8a8ab94d455c31e8b5229f3a8a98
Michal Szalkowski <info@michalszalkowski.com>
2020-11-13 01:48:20 +0100

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# javagda41_tyche
---
+++ src/test/java/com/michalszalkowski/TycheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.michalszalkowski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TycheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
