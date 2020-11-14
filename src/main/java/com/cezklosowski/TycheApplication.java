81fad95be49ceb935dcfa358afe260a9801cd55a
cezklosowski <cez.klosowski@gmail.com>
2020-11-14 10:35:34 +0100

    HomeController

--- src/main/java/com/cezklosowski/TycheApplication.java
+++ src/main/java/com/cezklosowski/TycheApplication.java
@@ -1,4 +1,4 @@
-package com.michalszalkowski;
+package com.cezklosowski;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/cezklosowski/TycheApplicationTests.java
+++ src/test/java/com/cezklosowski/TycheApplicationTests.java
@@ -1,4 +1,4 @@
-package com.michalszalkowski;
+package com.cezklosowski;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
